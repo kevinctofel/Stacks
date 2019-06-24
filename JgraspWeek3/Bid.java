29a83e671975e3750a7fb7f57560feed6fb71204
Kevin C. Tofel <ktofel3811@students.mc3.edu>
2019-06-24 17:18:55 -0400

    Fleshing out Bid object

--- JgraspWeek3/Bid.java
+++ JgraspWeek3/Bid.java
@@ -1,5 +1,36 @@
 
 
-public class Bid {
-
+public class Bid 
+{
+   protected String bidderName;
+   protected int maxBid, currentBid;
+   
+   // Constructor
+   
+   public Bid(String newBidderName, int newMaxBid, int newCurrentBid)
+   {
+      bidderName = newBidderName; maxBid = newMaxBid; currentBid = newCurrentBid;
+   }
+   
+   public String getName() {
+      return this.bidderName;
+   }
+   
+   public int getMaxBid() {
+      return this.maxBid;
+   }
+   
+   public int getCurrentBid() {
+      return this.currentBid;
+   }
+   
+   public void setName(String name) {
+      this.bidderName = name;
+   }
+   
+   public void setMaxBid(int maxBid) {
+      this.maxBid = maxBid;
+   }
+   
+   
 }
