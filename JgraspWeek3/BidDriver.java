8fbf88fffc0329da48b7b21c1d6ef8b62bb91171
Kevin C. Tofel <ktofel3811@students.mc3.edu>
2019-06-24 21:09:16 -0400

    Start of bid driver

--- JgraspWeek3/BidDriver.java
+++ JgraspWeek3/BidDriver.java
@@ -1,14 +1,29 @@
 import ch02.stacks.*;
-import ch02.support;
+import support.*;
 import java.util.Scanner;
 
-public class BidDriver 
+public class BidDriver<T> extends LinkedStack<T>
 {
 
    public static void main(String args[])
    {
    
-   
+      String newBidderName;
+      int newMaxBid;
+      int current = 1;     // Start the bidding at one
+      
+      Scanner keyboard = new Scanner(System.in);
+      
+      System.out.println("Enter a bidder's name: ");
+      newBidderName = keyboard.nextLine();
+      System.out.println("Enter the bidder's maximum bid: ");
+      newMaxBid = keyboard.nextInt();
+      
+      Bid first = new Bid(newBidderName, newMaxBid, 1);
+      
+      
+      
+      
    
    }
 
